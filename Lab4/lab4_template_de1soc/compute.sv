a18af6e7fd8d3da515927149555fb573bb677d5e
Dennis Truong <dennisnhatsontruong@gmail.com>
2020-03-03 20:53:15 -0800

    init and empty compute

---
+++ Lab4/lab4_template_de1soc/compute.sv
@@ -0,0 +1,15 @@
+module compute(
+    //inputs
+    start,
+    //outputs
+    data, address, wren, complete
+);
+
+input start;
+
+output [7:0] data;
+output [7:0] address;
+output wren;
+output complete;
+
+endmodule
