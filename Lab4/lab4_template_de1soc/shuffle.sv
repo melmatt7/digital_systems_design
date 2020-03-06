fe72587b5951d72accd00b55a361b468fa779d4a
Melvin Mathews <mel.matt007@gmail.com>
2020-03-05 16:25:35 -0800

    shuffle work

--- Lab4/lab4_template_de1soc/shuffle.sv
+++ Lab4/lab4_template_de1soc/shuffle.sv
@@ -8,44 +8,47 @@ module shuffle(//inputs
 			   output reg[7:0] address,
 			   output logic wren,
 			   output logic complete
-			  );
-			  
-	reg[7:0] state   = 8'b0000_0_0_0_0;						
+			  );				
 	reg[7:0] key_val = 8'b0;
 	reg[7:0] j = 8'b0;
 	reg[7:0] i = 8'b0;
 
 	reg[7:0] q_si = 8'b0;
 	reg[7:0] q_sj = 8'b0;
-								 //7654_3_2_1_0	
-	parameter idle     = 8'b0000_0_0_0_0;
-	parameter addr_i   = 8'b0001_0_1_0_0;
-	parameter read_i   = 8'b0010_0_1_0_0;
-	parameter calc_j   = 8'b0011_0_0_0_0;
-	parameter addr_j   = 8'b0100_0_0_0_0;
-	parameter read_j   = 8'b0101_0_0_0_0;
-	parameter swap_i   = 8'b0111_0_1_0_1;
-	parameter swap_j   = 8'b1000_0_0_1_1;
-	parameter check_i  = 8'b1001_0_0_0_0;
-	parameter incr_i   = 8'b1010_0_0_0_0;
-	parameter finish   = 8'b1011_1_0_0_0;
+						   //9876_5_4_3_2_1_0	
+	parameter idle     = 10'b0000_0_0_0_0_0_0;
+	parameter addr_i   = 10'b0001_0_0_0_1_0_0;
+	parameter read_i   = 10'b0010_0_1_0_0_0_0;
+	parameter calc_j   = 10'b0011_0_0_0_0_0_0;
+	parameter addr_j   = 10'b0100_0_0_0_0_0_0;
+	parameter read_j   = 10'b0101_1_0_0_0_0_0;
+	parameter swap_i   = 10'b0111_0_0_0_1_0_1;
+	parameter swap_j   = 10'b1000_0_0_0_0_1_1;
+	parameter check_i  = 10'b1001_0_0_0_0_0_0;
+	parameter incr_i   = 10'b1010_0_0_0_0_0_0;
+	parameter finish   = 10'b1011_0_0_1_0_0_0;
 	
+	reg[9:0] state   = idle;
+
 	assign wren = state[0];
-	assign data= state[1] ? q_si : q_sj;
+	assign data = state[1] ? q_si : q_sj;
 	assign address = state[2] ? i : j;
 	assign complete = state[3];
+
+	assign read_i_en = state[4];
+	assign read_j_en = state[5];
 
 	always_ff @(posedge clk)
 	begin
 		case(state) 
 			idle:
-				if(start) state <= read_i;
+				if(start) state <= addr_i;
 			addr_i:
 				state <= read_i;
 			read_i:
 				state <= calc_j;
 			calc_j:
-				state <= read_j;
+				state <= addr_j;
 			addr_j:
 				state <= read_j;
 			read_j:
@@ -58,7 +61,7 @@ module shuffle(//inputs
 				if(i > 8'hFE) state <= finish;
 				else state <= incr_i;
 			incr_i:
-				state <= read_i;
+				state <= addr_i;
 			finish:
 				state <= idle;
 			// default:
@@ -76,16 +79,12 @@ module shuffle(//inputs
 	
 	always_ff @(posedge clk)
 	begin
-		if(state == read_i) begin
-			q_si <= q;
-		end
+		if(read_i_en) q_si <= q;
 	end
 
 	always_ff @(posedge clk)
 	begin
-		if(state == read_j) begin
-			q_sj <= q;
-		end
+		if(read_j_en) q_sj <= q;
 	end
 	
 	always_ff @(posedge clk)
